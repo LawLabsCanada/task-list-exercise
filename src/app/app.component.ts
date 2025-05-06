953ff581fed5b3f718fe64b1387b7798cea3661d
nicholasuarus <nicholas.olsen@lawlabs.ca>
2025-05-06 16:50:09 -0400

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskListComponent } from './components/task-list/task-list.component';
-import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TaskListComponent, TaskFormComponent],
+  imports: [CommonModule],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'task-list';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { NgxsModule } from '@ngxs/store';
-import { TaskState } from './store/task.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(NgxsModule.forRoot([TaskState]))
   ]
 };
